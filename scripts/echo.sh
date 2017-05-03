69ca760425d7e27a6335bb4a0469ab4e75f8e11f
Ansuman Roy <ansumanroygit@gmail.com>
2017-05-03 13:45:37 +1000

    scripts updated to reflect master

--- scripts/echo.sh
+++ scripts/echo.sh
@@ -31,6 +31,11 @@ set +e
 source scripts/jobUpdater.properties
 _listOfJobsRequested=$1
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 ## look no git hub coding here :)
 #  validate if the job names exist in the branch
 cd jobs
@@ -48,10 +53,14 @@ do
 done
 
 #if this reaches this stage that means folder checks are done and good to create or update
-
+touch jobStatsFile.txt
 #loop through all the jobs
 for i in $(echo $_listOfJobsRequested | sed "s/,/ /g")
 do
+	
+	STATUS="NA"
+	COMMENT="NA"
+	
     #check if folder exists
     if [  -d "$i" ]; then
         echo "folder $i exists"
@@ -64,21 +73,58 @@ do
 
         #404 signifies that the config was not present, so will create it
         if grep -q "404" mylocalconfig.xml; then
-   			 echo "Jenkins job $i doesnt exist in the destination, CREATE "
+			 
+   			 printf "${GREEN}Jenkins job $i doesnt exist in the destination, CREATE ${NC} \n"
+			 STATUS="CREATED"
    			 curl -X POST -H "Content-Type:application/xml" -d @$i/config.xml $jenkins_protocol://$jenkins_admin_username:$jenkins_admin_token@$jenkins_hostname:$jenkins_port/createItem?name=$i
+			 
 		else
-			 #if no 404, then the config is present, so go ahead and update it
-    		 echo "Jenkins job exists with $i name in the destination, UPDATE"
-    		 curl -X POST $jenkins_protocol://$jenkins_admin_username:$jenkins_admin_token@$jenkins_hostname:$jenkins_port/job/$i/config.xml --data-binary "@$i/config.xml"
-
+			 #if no 404, then the config is present and job is not running, so go ahead and update it
+			 
+			 curl $jenkins_protocol://$jenkins_admin_username:$jenkins_admin_token@$jenkins_hostname:$jenkins_port/job/$i/lastBuild/api/json | grep --color result\":null
+			
+			 if [ $? -eq 0 ]; then
+				 printf "${RED}Jenkins job exists with $i name in the destination, but is currently Running, WILL NOT UPDATE${NC}\n"
+				 STATUS="NOT UPDATED"
+				 COMMENT="Job is still running so will not update."
+			 else
+	    		 printf "${GREEN}Jenkins job exists with $i name in the destination, UPDATE${NC}\n"
+	    		 curl -X POST $jenkins_protocol://$jenkins_admin_username:$jenkins_admin_token@$jenkins_hostname:$jenkins_port/job/$i/config.xml --data-binary "@$i/config.xml"
+				 STATUS="UPDATED"
+				 
+			 fi
+			 
+    		 
 		fi
 		#remove the temporary file
 		rm -rf mylocalconfig.xml
 
       
     fi
+	
+	if [[ $STATUS == "NOT UPDATED" ]]; then
+		printf "$i \t ${RED}$STATUS${NC} \t $COMMENT\n" >> jobStatsFile.txt
+	else
+		printf "$i \t ${GREEN}$STATUS${NC} \t $COMMENT\n" >> jobStatsFile.txt
+	fi
 
 done
+
+printf "${BLUE}######################## REPORT START ###############################${NC}\n"
+cat jobStatsFile.txt
+printf "${BLUE}######################## REPORT END   ###############################${NC}\n"
+rm jobStatsFile.txt
+
+#USAGE printf "I ${RED}love${NC} Stack Overflow\n"
+#Reference of Colour Codes
+#Black        0;30     Dark Gray     1;30
+#Red          0;31     Light Red     1;31
+#Green        0;32     Light Green   1;32
+#Brown/Orange 0;33     Yellow        1;33
+#Blue         0;34     Light Blue    1;34
+#Purple       0;35     Light Purple  1;35
+#Cyan         0;36     Light Cyan    1;36
+#Light Gray   0;37     White         1;37
 
 
 
